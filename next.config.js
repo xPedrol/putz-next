2c2564428f4be4236f2b5da8a2f0eebe56b9612c
PedroL <pedrolucassantosferreira426@gmail.com>
2021-12-12 15:16:24 -0300

    .

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    env: {
+        production: true,
+        API_URL: 'https://api.putzfilmes.com/api/',
+        IMAGE_URL: 'https://api.putzfilmes.com/api/storage',
+        RENDER_URL: 'https://render.putzfilmes.com/',
+        RENDER_KEY: 'myapisecret',
+        PORT: '8090'
+    },
+};
